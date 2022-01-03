fe5b0e58a03e7b8c3c950f859c97eb040d54948b
luismiguwl <luisbwrb@gmail.com>
2022-01-03 07:55:39 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/luis/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
