dda08ba78dd78ad7fe7d6c64b17effdb458bec77
luismiguwl <luisbwrb@gmail.com>
2022-01-05 13:07:27 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/luis/hroauth/entities/Role.java
@@ -0,0 +1,46 @@
+package com.luis.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String roleName;
+
+	public Role() {
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(roleName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(roleName, other.roleName);
+	}
+
+}
---
+++ hr-oauth/src/main/java/com/luis/hroauth/entities/User.java
@@ -0,0 +1,49 @@
+package com.luis.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	
+	private String email;
+	private String password;
+
+	private Set<Role> roles = new HashSet<>();
+
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+}
